0e902abe8f17845b34cfc7689bfb64dfe4c733db
Israfil Hossen <sisrafilss@gmail.com>
2023-04-24 06:21:46 +0600

    dependencies version updated

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
-const express = require('express');
-const { MongoClient } = require('mongodb');
-const cors = require('cors');
-require('dotenv').config()
+const express = require("express");
+const { MongoClient } = require("mongodb");
+const ObjectId = require("mongodb").ObjectId;
+const cors = require("cors");
+require("dotenv").config();
 
 const app = express();
 const port = process.env.PORT || 5000;
@@ -10,27 +11,29 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-
-
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.quv1r.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
-const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+const uri = `mongodb://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0-shard-00-00.quv1r.mongodb.net:27017,cluster0-shard-00-01.quv1r.mongodb.net:27017,cluster0-shard-00-02.quv1r.mongodb.net:27017/myFirstDatabase?ssl=true&replicaSet=atlas-teugro-shard-0&authSource=admin&retryWrites=true&w=majority`;
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
 
 async function run() {
-    try {
-        await client.connect();
+  try {
+    await client.connect();
 
-        console.log('database connected successfully');
-    }
-    finally {
-        // await client.close();
-    }
+    const database = client.db("db_name");
+    const collectionName = database.collection("collectionName");
+    
+  } finally {
+    // await client.close();
+  }
 }
-run().catch(console.dir)
+run().catch(console.dir);
 
-app.get('/', (req, res) => {
-    res.send('Simple Express Server is Running');
-})
+app.get("/", (req, res) => {
+  res.send("Simple ExpressJS server is running...");
+});
 
 app.listen(port, () => {
-    console.log('Server has started at port:', port);
-})
+  console.log("Server has started at port", port);
+});
